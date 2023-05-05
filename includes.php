123ab80948a8fd160d8b48c3bf7487967d0a910b
Anderson Grüdtner Martins <andergmartins@gmail.com>
2023-05-05 15:01:13 -0300

    Update includes.php

--- includes.php
+++ includes.php
@@ -1,5 +1,5 @@
 <?php
 
-if (! defined('PUBLISHPRESS_CHECKLISTS_VENDOR_PATH')) {
-    define('PUBLISHPRESS_CHECKLISTS_VENDOR_PATH', realpath(__DIR__ . '/../../'));
+if (! defined('PUBLISHPRESS_CHECKLISTS_PRO_VENDOR_PATH')) {
+    define('PUBLISHPRESS_CHECKLISTS_PRO_VENDOR_PATH', realpath(__DIR__ . '/../../'));
 }
